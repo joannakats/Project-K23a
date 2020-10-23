6c1afdba49ab551436ead5ec4f8ae40d853eb6b3
joannakats <katsoulijoanna@gmail.com>
2020-10-23 18:17:52 +0300

    declaration of hashtable functions and struct

---
+++ include/hashtable.h
@@ -0,0 +1,12 @@
+typedef struct hashtable {
+    int index;
+    node* head;
+
+}hashtable;
+
+
+hashtable *hashtable_init(int tableSize);
+//hash function ( djb2 from http://www.cse.yorku.ca/~oz/hash.html)
+unsigned  long hash(const char* key,int tableSize);
+void insert_entry(hashtable *hsTable,int key,char* data); 
+int delete_hashtable(hashtable *hsTable,int tableSize);
