8ee72671f40cc17318347a35d0e3f22fc9b5cab7
joannakats <katsoulijoanna@gmail.com>
2020-10-24 20:21:59 +0300

    update hashtable changes

--- src/hashtable.c
+++ src/hashtable.c
@@ -5,11 +5,12 @@
 #include "spec.h"
 
 //needs testing and to add insert_entry
-hashtable *hashtable_init(int tableSize){
-	hashtable *hsTable= malloc(tableSize*sizeof(hashtable));
-	for(int i=0;i<tableSize;i++){
-		hsTable[i].index=i;
-		hsTable[i].head=NULL;
+hashtable hashtable_init(int size){
+	hashtable hsTable;
+	hsTable.tableSize=size;
+	hsTable.list= malloc(size*sizeof(node*));
+	for(int i=0;i<size;i++){
+		hsTable.list[i]=NULL;
     }
     return hsTable;
 }
@@ -28,19 +29,20 @@ unsigned  long  hash(const char* key,int tableSize){
 
 void insert_entry(hashtable* hsTable,int index,char* spec,int numOfFields,char **properties,char **values){// 0<=index<tableSize
 	node* temp;
-	if(hsTable[index].head==NULL){
-		hsTable[index].head=spec_insert(hsTable[index].head,spec,numOfFields,properties,values);
+	if(hsTable->list[index]==NULL){
+		hsTable->list[index]=spec_insert(hsTable->list[index],spec,numOfFields,properties,values);
 	}else{
-		temp=spec_insert(hsTable[index].head,spec,numOfFields,properties,values);
+		temp=spec_insert(hsTable->list[index],spec,numOfFields,properties,values);
 	}
 
 }
 
 
-int delete_hashtable(hashtable *hsTable,int tableSize){
-	for(int i=0;i<tableSize;i++){
-		delete_specList(hsTable[i].head);
+
+int delete_hashtable(hashtable *hsTable){
+	int size=hsTable->tableSize;
+	for(int i=0;i<size;i++){
+		delete_specList(hsTable->list[i]);
 	}
-	free(hsTable);
 	return 0;
 }
