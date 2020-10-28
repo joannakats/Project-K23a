50dfe60f10ba4997228a7170625d19406316da30
marinaKnm <konomimarina830@gmail.com>
2020-10-28 03:08:38 +0200

    test new spec

--- tests/test_spec.c
+++ tests/test_spec.c
@@ -6,44 +6,27 @@
 node *head = NULL;
 
 int main() {
-	char **prop1, **prop2, **prop3, **v1, **v2, **v3;
-	int cnt1=3, cnt2=2, cnt3=1;
 	int i;
 	char *str="This is a property.";
 	char *s="This a value.";
 	char *id="This is an id.";
-	prop1 = malloc(cnt1*sizeof(char*));
-	prop2 = malloc(cnt2*sizeof(char*));
-	prop3 = malloc(cnt3*sizeof(char*));
-	v1 = malloc(cnt1*sizeof(char*));
-	v2 = malloc(cnt2*sizeof(char*));
-	v3 = malloc(cnt3*sizeof(char*));
 
-
-	for(i=0; i<cnt1; i++) {
-		prop1[i] = malloc(sizeof(char)*(strlen(str)+1));
-		v1[i] = malloc(sizeof(char)*(strlen(s)+1));
-		strcpy(prop1[i], str);
-		strcpy(v1[i], s);
+	field *f = createFieldArray(1);
+	setField(f, 2, str);
+	for(i=0; i<2; i++) {
+		setValue(f, i, s);
 	}
 
-	for(i=0; i<cnt2; i++) {
-		prop2[i] = malloc(sizeof(char)*(strlen(str)+1));
-		v2[i] = malloc(sizeof(char)*(strlen(s)+1));
-		strcpy(prop2[i], str);
-		strcpy(v2[i], s);
-	}
+	head = spec_insert(head, id, f, 1);
 
-	for(i=0; i<cnt3; i++) {
-		prop3[i] = malloc(sizeof(char)*(strlen(str)+1));
-		v3[i] = malloc(sizeof(char)*(strlen(s)+1));
-		strcpy(prop3[i], str);
-		strcpy(v3[i], s);
-	}
+	field *f1 = createFieldArray(2); //2 pairs property-numOfValues
+	setField(&f1[0], 1, str);
+	setValue(&f1[0], 0, s);
+	setField(&f1[1], 2, str);
+	setValue(&f1[1], 0, s);
+	setValue(&f1[1], 1, s);
 
-	head = spec_insert(head, id,  cnt1, prop1, v1);
-	node  *temp = spec_insert(head, id,  cnt2, prop2, v2);
-	temp = spec_insert(head, id,  cnt3, prop3, v3);
+	node  *temp = spec_insert(head, id,  f1, 2);
 
 	print_list(head);
 
