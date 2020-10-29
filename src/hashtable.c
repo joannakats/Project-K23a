ba57b4bb1d1a56f9d94909f22c51a65b9b7eabbe
joannakats <katsoulijoanna@gmail.com>
2020-10-29 10:04:21 +0200

    hashtable_search and join

--- src/hashtable.c
+++ src/hashtable.c
@@ -34,6 +34,35 @@ void insert_entry(hashtable* hsTable,char* id,field *fieldsArray,int numOfFields
 		hsTable->list[index] = temp;
 }
 
+node *search_hashTable_spec(hashtable* hsTable,char* id,int *pos){
+	int index=hash(id,hsTable->tableSize);
+	node *found=search_spec(hsTable->list[index],id,pos);
+	return found;
+}
+
+
+/* after the execution of this function, if both specs exist in the hashtable one of the following will be done :
+if  specs are in the same list, the clique list will be rearranged in first node on the list.
+if specs are in different lists on the hashtable,the clique will be rearranged in first spec on the hashtable.
+(the other spec will point to the first spec's list of clique nodes)
+*/ 
+void hash_table_join(hashtable* hsTable,char* left_id,char* right_id){
+	int left_pos,right_pos;
+	int leftIndex=hash(left_id,hsTable->tableSize);
+	int rightIndex=hash(right_id,hsTable->tableSize);
+	node *left_spec=search_hashTable_spec(hsTable,left_id,&left_pos);
+	node *right_spec=search_hashTable_spec(hsTable,right_id,&right_pos);
+
+	//both spec_ids where found
+	if(left_pos!=-1 && right_pos!=-1){
+		if( (leftIndex==rightIndex && left_pos<right_pos) || (leftIndex<rightIndex) ){
+				clique_rearrange(left_spec,right_spec);
+		}else{
+				clique_rearrange(right_spec,left_spec);
+		}		
+	}
+}
+
 
 int delete_hashtable(hashtable *hsTable){
 	int size=hsTable->tableSize;
