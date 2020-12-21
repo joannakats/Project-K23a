61ba8a31a8eb6d296b8fa7190270ca0d59df9786
joannakats <katsoulijoanna@gmail.com>
2020-12-21 21:26:44 +0200

    loregression train,predict

--- include/loregression.h
+++ include/loregression.h
@@ -6,17 +6,14 @@
 typedef struct logistic_regression{
 	int size;
 	double b;
-    double *w;
-
+	double *w;
+	
 }logistic_regression;
-
 
 logistic_regression *loregression_init(int voc_size);
 void loregression_delete(logistic_regression *loregression);
-int logisticregression_train(logistic_regression *loregression,node *spec1,node *spec2,int label);
-int loregression_weight(logistic_regression *loregression,node *spec1,node *spec2);//
-void loregression_predict(logistic_regression *loregression,double *j,double *y);
-
-
+int logisticregression_train(logistic_regression *loregression,node *spec1,node *spec2,double label);
+double sigmoid(double x);
+double loregression_predict(logistic_regression *loregression,node *spec_left,node *spec_right);
 
 #endif /* LOREGRESSION_H aka logistic regression */
--- src/loregression.c
+++ src/loregression.c
@@ -32,25 +32,81 @@ void loregression_delete(logistic_regression *loregression){
 	free(loregression->w);
 	free(loregression); //
 }
+ double sigmoid(double x){
+     return 1.0/(1.0 +exp(-x));
+ }
 
-int loregression_train(logistic_regression *loregression,node *spec1,node *spec2,double label){
-//TODO:train
 
 
+/* The goal is to find the regression accounting function p(x) ,p(xi) near 0 or 1. */
+int loregression_train(logistic_regression *loregression,node *spec_left,node *spec_right,double label){
+	int word_size=spec_left->wordCount + spec_right->wordCount;
+	int i,index=0,k;
+	double *f=malloc(word_size*sizeof(*f));
+	double *x=malloc(word_size*sizeof(*x));
+	for(int i=0;i<word_size;i++){
+		f[i]=0; //initialize with 0 for the following sum
+	}
+	//concat spec_idf_factors in x
+	for(i=0;i<spec_left->wordCount;i++){
+		x[index]=spec_left->tf_idf_factors[i];
+		index++;
+	}
+	for(i=0;i<spec_right->wordCount;i++){
+		x[index]=spec_right->tf_idf_factors[i];
+		index++;
+	}
+	
+	//compute f(i)=w(i)*x[i]+b
+	for(i=0;i<word_size-2;i++){
+		f[i]+=loregression->w[i]*x[i]; /* TODO check weights ??*/
+	}
+	for(i=0;i<word_size;i++){
+		f[i]+=loregression->b;
+	}
+	
+	//p(x)=sigmoid(f(x))
+	for(i=0;i<word_size;i++){
+		f[i]=sigmoid(f[i]);
+	}
+	//cost 
+	double *j=malloc(word_size*sizeof(double));
+	for(k=0;k<word_size;k++){
+		j[k]=0;
+	}
+	for( k=0;k<word_size;k++){
+		j[k]+=(f[k]-label)*x[k];
+	}
+	//weigh 
+	for( k=0;k<word_size;k++){ 
+		loregression->w[k]-=LR*j[k];
+	}
+	
+	free(f);
+	free(x);
 	return 0;
-
 }
 
-int loregression_weight(logistic_regression *loregression,node *spec1,node *spec2){
 
-	//TODO:lore_weight
 
-	return 0;
-
-}
-
-void loregression_predict(logistic_regression *loregression,double *j,double *y){
-
-//TODO:predict
-
+double loregression_predict(logistic_regression *loregression,node *spec_left,node *spec_right){
+	double *x=malloc((spec_left->wordCount + spec_right->wordCount )*sizeof(*x));
+	int i,index=0;
+	double pred = loregression->b; //or 0 for testing??
+	//concat spec_idf_factors in x
+	for(i=0;i<spec_left->wordCount;i++){
+		x[index]=spec_left->tf_idf_factors[i];
+		index++;
+	}
+	for(i=0;i<spec_right->wordCount;i++){
+		x[index]=spec_right->tf_idf_factors[i];
+		index++;
+	}
+	for(int j=0;j<(index-1);j++){
+		pred+=loregression->w[j] *x[j] ;
+	}
+	
+	
+	free(x);
+	return sigmoid(pred);
 }
