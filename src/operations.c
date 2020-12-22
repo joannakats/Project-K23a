4fed13e328c5b44babb69fbf13bcdc5abeb5d61f
marinaKnm <konomimarina830@gmail.com>
2020-12-22 23:50:11 +0200

    "typo in comment"

--- src/operations.c
+++ src/operations.c
@@ -216,7 +216,7 @@ int begin_operations(int entries, char *dataset_x, char *dataset_w, char *output
 
 			fputs("[Reading Dataset W]\n", stderr);
 			if (!(ret = insert_dataset_w(&hash_table, dataset_w))) {
-				// comment out lines 220,221 and comment lines 223-229
+				// comment out lines 220,221 and comment lines 223-228
 				// int w = print_ground_truth(&hash_table);
 				// printf("num of pairs: %d\n", w);
 
