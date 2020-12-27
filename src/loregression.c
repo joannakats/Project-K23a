8858db995fea9b3b0db9016e8023fa62f78c8c42
joannakats <katsoulijoanna@gmail.com>
2020-12-27 14:49:02 +0200

    improvements in loregression_train and predict

--- src/loregression.c
+++ src/loregression.c
@@ -36,78 +36,54 @@ void loregression_delete(logistic_regression *loregression){
      return 1.0/(1.0 +exp(-x));
  }
 
+static double *make_x(node *spec_left,node *spec_right,int size){
 
+	double *x=malloc(size *sizeof(double));
+	for(int i=0;i<size;i++){
+		x[i]=fabs(spec_left->tf_idf_factors[i] - spec_right->tf_idf_factors[i]);
+	}
+	return x;
+}
 
 /* The goal is to find the regression accounting function p(x) ,p(xi) near 0 or 1. */
 int logregression_train(logistic_regression *loregression,node *spec_left,node *spec_right,double label){
-	int word_size=loregression->size;
-	int i,index=0,k;
-	double *f=malloc(word_size*sizeof(*f));
-	double *x=malloc(word_size*sizeof(*x));
-	for(int i=0;i<word_size;i++){
-		f[i]=0; //initialize with 0 for the following sum
-	}
-	//concat spec_idf_factors in x
-	for(i=0;i<spec_left->wordCount;i++){
-		x[index]=spec_left->tf_idf_factors[i];
-		index++;
-	}
-	for(i=0;i<spec_right->wordCount;i++){
-		x[index]=spec_right->tf_idf_factors[i];
-		index++;
-	}
-	for(i=index;i<word_size;i++){
-		x[i]=0.0;
-	}
-
+	int size=loregression->size;
+	double f= loregression->b;
+	double *x;
+	double loss_j,pred;
+	
+	x=make_x(spec_left,spec_right,size);
+	
 	//compute f(i)=w(i)*x[i]+b
-	for(i=0;i<word_size;i++){
-		f[i]+=loregression->w[i]*x[i]; /* TODO check weights ??*/
+	for(int i=0;i<size;i++){
+		f+=loregression->w[i]*x[i]; 
 	}
-	for(i=0;i<word_size;i++){
-		f[i]+=loregression->b;
-	}
-
+	
 	//p(x)=sigmoid(f(x))
-	for(i=0;i<word_size;i++){
-			f[i]=sigmoid(f[i]);
-
-	}
-	//cost
-	double *j=malloc(word_size*sizeof(double));
-	for(k=0;k<word_size;k++){
-		j[k]=0;
-	}
-	for( k=0;k<word_size;k++){
-		j[k]+=(f[k]-label)*x[k];
-	}
-	//weigh
-	for( k=0;k<word_size;k++){
-		loregression->w[k]-=LR*j[k];
+	pred=sigmoid(f);
+	
+	for(int k=0;k<size;k++){
+		//cost
+		loss_j=( pred -label)*x[k];
+		//weight
+		loregression->w[k]-= LR*loss_j;
 	}
 
-	free(f);
 	free(x);
 	return 0;
 }
 
 
 
+
 double loregression_predict(logistic_regression *loregression,node *spec_left,node *spec_right){
 	int size=loregression->size;
-	double *x=malloc((size )*sizeof(*x));
-	int i,index=0;
+	double *x;
 	double pred = loregression->b;
-	//concat spec_idf_factors in x
-	for(i=0;i<spec_left->wordCount;i++){
-		x[index]=spec_left->tf_idf_factors[i];
-		index++;
-	}
-	for(i=0;i<spec_right->wordCount;i++){
-		x[index]=spec_right->tf_idf_factors[i];
-		index++;
-	}
-	for(int j=index;j<size ;j++){
+	
+	x=make_x(spec_left,spec_right,size);
+	
+	for(int j=0;j<size ;j++){
 		pred+=loregression->w[j] *x[j] ;
 	}
 
