163170832d0a4a568d5cfe3582c7d6890ef77d70
joannakats <katsoulijoanna@gmail.com>
2021-01-14 19:40:04 +0200

    Create thread_pool.h

---
+++ include/thread_pool.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "queue.h"
+
+typedef struct JobScheduler{
+	int execution_threads; //number of execution threads
+	Queue q; //a queue that holds submitted jobs/tasks
+	pthread_t* tids;// execution threads //may have a diff type
+	pthread_mutex_t mutexQ;
+	pthread_cond_t condQ;	
+}JobScheduler;
+
+
+void jobsch_init(int threadNumber); 
+void executeJob(Queue* q,Task* task); //TODO 
+void* thread_function(void* args); //
+void enqueueJob(Queue* q,int* start, int* end);
