66d481c9ffd0d97bba788ab47d377c473c57e8d3
marinaKnm <konomimarina830@gmail.com>
2021-01-24 21:50:35 +0200

    fix

--- include/loregression.h
+++ include/loregression.h
@@ -18,12 +18,12 @@ double sigmoid(double x);
 void loregression_loss(logistic_regression *loregression, struct line *batch, long a, long b);
 void loregression_update_weights(logistic_regression *loregression, struct line *batch, long batch_size);
 int loregression_predict(logistic_regression *loregression,node *spec_left,node *spec_right);
-<<<<<<< HEAD
+
 double loregression_possibility(logistic_regression *loregression,node *spec_left,node *spec_right);
 void update_weights_of_clique(logistic_regression *model, clique *c);
 void loregression_update_weights_of_pair(logistic_regression *model, node *spec1, node *spec2, double label);
-=======
+
 /* Calculate hits for batch */
 long loregression_pbatch(logistic_regression *loregression,struct line* batch,long a,long b);
->>>>>>> 91d42321479d1e86884e9b1145bec5b8448bc6f3
+
 #endif /* LOREGRESSION_H aka logistic regression */
